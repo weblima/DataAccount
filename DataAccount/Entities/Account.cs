ab26e07eb979a5fc57a8ce539d248b0b40d4f627
Rafael Lima <rafael_weblima@hotmail.com>
2020-11-23 15:24:38 -0300

    Implementação das Entidades e classe Exception

---
+++ DataAccount/Entities/Account.cs
@@ -0,0 +1,37 @@
+﻿using DataAccount.Entities.Exceptions;
+
+namespace DataAccount.Entities {
+    class Account {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+        public Account() {
+        }
+        public Account(int number, string holder, double balance, double withDrawLimit) {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double amount) {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount) {
+
+            if (amount > WithDrawLimit) {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+
+            if (amount > Balance) {
+                throw new DomainException("Not enough balance");
+            }
+
+            Balance -= amount;
+        }
+
+    }
+}
---
+++ DataAccount/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataAccount.Entities.Exceptions {
+    class DomainException : ApplicationException {
+        public DomainException(string message) : base(message) {
+        }
+    }
+}
